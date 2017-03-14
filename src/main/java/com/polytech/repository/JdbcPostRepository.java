fe8bbdaa79dcbef3680dcea93bd356e402a9e5c2
KHOA <ddkhoa12pfiev3@gmail.com>
2017-03-14 09:36:41 +0100

    commit 4

---
+++ src/main/java/com/polytech/repository/JdbcPostRepository.java
@@ -0,0 +1,45 @@
+package com.polytech.repository;
+
+import com.polytech.business.Post;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by KHOA on 13-Mar-17.
+ */
+public class JdbcPostRepository implements PostRepository{
+
+    private final DataSource dataSource;  //constant -> forcer a creer un constructor
+
+    public JdbcPostRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void save(Post post) {
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO POST (CONTENT) VALUES (?)");
+            preparedStatement.setString(1,post.getContent());
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Post> findAll() {
+        List<Post> posts = new ArrayList<Post>();
+        try {
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM POST");
+            while(rs.next()){
+                posts.add(new Post(rs.getString(2)));
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return posts;
+    }
+}
