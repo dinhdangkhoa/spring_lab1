540499c786504bca9f3a2d40eefedea7a6fb394b
KHOA <ddkhoa12pfiev3@gmail.com>
2017-03-17 10:16:54 +0100

    commit remove plugin apache

--- src/main/java/com/polytech/config/SecurityConfig.java
+++ src/main/java/com/polytech/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                     .formLogin()
+                    .loginPage("/login")
                 .and()
                     .httpBasic()
                 .and()
